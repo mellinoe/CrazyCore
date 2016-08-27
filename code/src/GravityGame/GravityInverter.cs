92ed5b7350187cd649033437caa6a31ea7612783
Eric Mellino <erme@microsoft.com>
2016-08-26 19:11:01 -0700

    Import Engine as a submodule

---
+++ code/src/GravityGame/GravityInverter.cs
@@ -0,0 +1,43 @@
+﻿using Engine.Behaviors;
+using Engine;
+using Engine.Physics;
+using System;
+using Engine.Graphics;
+using System.Numerics;
+
+namespace GravityGame
+{
+    public class GravityInverter : Behavior
+    {
+        private InputSystem _input;
+        private PhysicsSystem _physics;
+        private GraphicsSystem _gs;
+
+        protected override void Start(SystemRegistry registry)
+        {
+            _input = registry.GetSystem<InputSystem>();
+            _physics = registry.GetSystem<PhysicsSystem>();
+            _gs = registry.GetSystem<GraphicsSystem>();
+        }
+
+        public override void Update(float deltaSeconds)
+        {
+            if (_input.GetKeyDown(Veldrid.Platform.Key.G))
+            {
+                InvertGravity();
+            }
+        }
+
+        private void InvertGravity()
+        {
+            _physics.Space.ForceUpdater.Gravity = -_physics.Space.ForceUpdater.Gravity;
+            
+            foreach (var entity in _physics.Space.Entities)
+            {
+                entity.ActivityInformation.Activate();
+            }
+
+            _gs.MainCamera.UpDirection = Vector3.Normalize(-_physics.Space.ForceUpdater.Gravity);
+        }
+    }
+}
